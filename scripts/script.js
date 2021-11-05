4738877ce041f48dafd420649869947f9d6c36b6
Michael Montgomery <thehealthyprogrammer@outlook.com>
2021-11-05 13:46:08 -0400

    fdsfds

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+
+document.getElementById('mainPictureWrapper').style.opacity = '1';
+
+setTimeout(function () {
+    document.getElementById('headerWrapper').style.height = '70px';
+    document.getElementById('mainPictureWrapper').style.height = '80vh';
+
+}, 2000)
+
+
+document.getElementById('sendMessageButton').addEventListener('click', function() {
+    alert('Go fuck yourself!');
+})
+
